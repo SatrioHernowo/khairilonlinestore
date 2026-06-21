e9fc50b9b5511a5379bb3e4f014b001586b1f798
SatrioHernowo <owogeming@gmail.com>
2026-06-21 19:37:08 +0700

    perubahan css3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
-  <link href="{{ asset('public/css/app.css') }}" rel="stylesheet" />
-  <title>@yield('title', 'Online Store')</title>
+<link href="{{ asset('css/app.css') }}?v={{ time() }}" rel="stylesheet">  <title>@yield('title', 'Online Store')</title>
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
