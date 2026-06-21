3ad5c92bda1711519f6ffb5bbdec5d46cfa2cff8
SatrioHernowo <owogeming@gmail.com>
2026-06-21 21:57:42 +0700

    appblade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
-<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}" rel="stylesheet" />
   <title>@yield('title', 'Admin - Online Store')</title>
 </head>
 <body>
